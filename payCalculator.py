c60507c259d3be0ee74cb49f14a0d0f82f7429bf
Kristina Palmer <Kristina.Palmer@vu.com>
2022-01-28 10:00:10 -0600

    First Attempt

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,18 @@ def calculatePay():
     
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+    h = float(hrs)
+    rate = input("Enter Rate:")
+    pay = float(rate)
+    if h <=40:
+        TotalPay=(h*pay)
+    elif h > 40:
+        TotalPay=(40*pay) + (h-40)*1.5*pay
+
+    print("Enter Hours:" +" "+ hrs)
+    print("Enter Rate:" +" "+ rate)
+    print ("Pay:" , TotalPay)
+
     # end assignment
 
 ## if you want to test locally before you try to sync
